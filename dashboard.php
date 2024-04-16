555adfa690a088f8e50caa3b9650d3efa0bc017c
KimMancera <mancerakim2003@gmail.com>
2024-04-16 23:32:47 +0800

    dashboard

---
+++ dashboard.php
@@ -0,0 +1,294 @@
+<?php
+session_start();
+
+// Check if the user is not logged in, then redirect to login page
+if (!isset($_SESSION['username'])) {
+    header('Location: loginform.php');
+    exit;
+}
+
+// User's username for a personalized message (optional)
+$username = isset($_SESSION['username']) ? $_SESSION['username'] : '';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>AdminLTE Interface</title>
+  <!-- AdminLTE CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/css/adminlte.min.css">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <style>
+    .edit-profile-form {
+      display: none;
+    }
+  </style>
+</head>
+<body class="hold-transition sidebar-mini">
+  <div class="wrapper">
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+        <!-- Home Button -->
+        <li class="nav-item d-none d-sm-inline-block">
+          <a href="#" class="nav-link">Home</a>
+        </li>
+      </ul>
+      <!-- Right navbar links -->
+      <ul class="navbar-nav ml-auto">
+        <!-- Messages Dropdown Menu -->
+        <li class="nav-item dropdown">
+          <a class="nav-link" data-toggle="dropdown" href="#">
+            <i class="far fa-comments"></i>
+            <span class="badge badge-danger navbar-badge">3</span>
+          </a>
+          <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+            <a href="#" class="dropdown-item">
+              <!-- Message Start -->
+              <div class="media">
+                <div class="media-body">
+                  <h3 class="dropdown-item-title">
+                    Brad Diesel
+                  </h3>
+                  <p class="text-sm">Call me whenever you can...</p>
+                </div>
+              </div>
+              <!-- Message End -->
+            </a>
+          </div>
+        </li>
+      </ul>
+    </nav>
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <!-- Brand Logo -->
+      <a href="#" class="brand-link">
+        <span class="brand-text font-weight-light">AdminLTE 3</span>
+      </a>
+      <!-- Sidebar -->
+      <div class="sidebar">
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Profile Link -->
+            <li class="nav-item">
+              <a href="#" class="nav-link" id="profile-btn">
+                <i class="nav-icon fas fa-user"></i>
+                <p>
+                  Profile
+                </p>
+              </a>
+            </li>
+            <!-- Sign Out Link -->
+            <li class="nav-item">
+              <a href="logout.php" class="nav-link" id="sign-out-btn">
+                <i class="nav-icon fas fa-sign-out-alt"></i>
+                <p>
+                  Sign Out
+                </p>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+      </div>
+      <!-- /.sidebar -->
+    </aside>
+    <!-- Content Wrapper. Contains page content -->
+    <section class="content-header">
+                    <div class="container-fluid">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1><?php echo $row->name; ?> Profile</h1>
+                            </div>
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="pages_dashboard.php">Dashboard</a></li>
+                                    <li class="breadcrumb-item"><a href="pages_manage.php">iBanking Staffs</a></li>
+                                    <li class="breadcrumb-item"><a href="pages_manage.php">Manage</a></li>
+                                    <li class="breadcrumb-item active"><?php echo $row->name; ?></li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div><!-- /.container-fluid -->
+                </section>
+    <div class="content-wrapper">
+    <section class="content">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-3">
+
+                                <!-- Profile Image -->
+                                <div class="card card-purple card-outline">
+                                    <div class="card-body box-profile">
+                                        <div class="text-center">
+                                            <?php echo $profile_picture; ?>
+                                        </div>
+
+                                        <h3 class="profile-username text-center"><?php echo $row->name; ?></h3>
+
+                                        <p class="text-muted text-center">Staff @iBanking </p>
+
+                                        <ul class="list-group list-group-unbordered mb-3">
+                                            <li class="list-group-item">
+                                                <b>Email: </b> <a class="float-right"><?php echo $row->email; ?></a>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <b>Phone: </b> <a class="float-right"><?php echo $row->phone; ?></a>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <b>StaffNo: </b> <a class="float-right"><?php echo $row->staff_number; ?></a>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <b>Gender: </b> <a class="float-right"><?php echo $row->sex; ?></a>
+                                            </li>
+
+                                        </ul>
+
+                                    </div>
+                                    <!-- /.card-body -->
+                                </div>
+                                
+                            </div>
+
+                            <!-- /.col -->
+                            <div class="col-md-9">
+                                <div class="card">
+                                    <div class="card-header p-2">
+                                        <ul class="nav nav-pills">
+                                            <li class="nav-item"><a class="nav-link active" href="#update_Profile" data-toggle="tab">Update Profile</a></li>
+                                            <li class="nav-item"><a class="nav-link" href="#Change_Password" data-toggle="tab">Change Password</a></li>
+                                        </ul>
+                                    </div><!-- /.card-header -->
+                                    <div class="card-body">
+                                        <div class="tab-content">
+                                            <!-- / Update Profile -->
+                                            <div class="tab-pane active" id="update_Profile">
+                                                <form method="post" enctype="multipart/form-data" class="form-horizontal">
+                                                    <div class="form-group row">
+                                                        <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" name="name" required class="form-control" value="<?php echo $row->name; ?>" id="inputName">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="email" name="email" required value="<?php echo $row->email; ?>" class="form-control" id="inputEmail">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="inputName2" class="col-sm-2 col-form-label">Contact</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" class="form-control" required name="phone" value="<?php echo $row->phone; ?>" id="inputName2">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="inputName2" class="col-sm-2 col-form-label">Profile Picture</label>
+                                                        <div class="input-group col-sm-10">
+                                                            <div class="custom-file">
+                                                                <input type="file" name="profile_pic" class=" form-control custom-file-input" id="exampleInputFile">
+                                                                <label class="custom-file-label  col-form-label" for="exampleInputFile">Choose file</label>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="inputName2" class="col-sm-2 col-form-label">Gender</label>
+                                                        <div class="col-sm-10">
+                                                            <select class="form-control" name="sex">
+                                                                <option>Male</option>
+                                                                <option>Female</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <div class="offset-sm-2 col-sm-10">
+                                                            <button name="update_staff_account" type="submit" class="btn btn-outline-success">Update Account</button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+
+                                            <!-- /Change Password -->
+                                            <div class="tab-pane" id="Change_Password">
+                                                <form method="post" class="form-horizontal">
+                                                    <div class="form-group row">
+                                                        <label for="inputName" class="col-sm-2 col-form-label">Old Password</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="password" class="form-control" required id="inputName">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="inputEmail" class="col-sm-2 col-form-label">New Password</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="password" name="password" class="form-control" required id="inputEmail">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="inputName2" class="col-sm-2 col-form-label">Confirm New Password</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="password" class="form-control" required id="inputName2">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <div class="offset-sm-2 col-sm-10">
+                                                            <button type="submit" name="change_staff_password" class="btn btn-outline-success">Change Password</button>
+                                                        </div>
+                                                    </div>
+
+                                                </form>
+                                            </div>
+                                            <!-- /.tab-pane -->
+                                        </div>
+                                        <!-- /.tab-content -->
+                                    </div><!-- /.card-body -->
+                                </div>
+                                <!-- /.nav-tabs-custom -->
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <!-- /.row -->
+                    </div><!-- /.container-fluid -->
+                </section>
+    </div>
+    <!-- /.content-wrapper -->
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+    </aside>
+    <!-- /.control-sidebar -->
+    <!-- Main Footer -->
+    <footer class="main-footer">
+      <!-- To the right -->
+      <div class="float-right d-none d-sm-inline">
+        Anything you want
+      </div>
+      <!-- Default to the left -->
+      <strong>Footer &copy; 2024 <a href="#">AdminLTE.io</a>.</strong> All rights reserved.
+    </footer>
+  </div>
+  <!-- jQuery -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/js/adminlte.min.js"></script>
+  <script>
+    $(document).ready(function() {
+        
+        // Handle profile button click
+        $('#profile-btn').click(function(e) {
+            e.preventDefault();
+            // Toggle the visibility of the edit profile form
+            $('.edit-profile-form').toggle();
+        });
+    });
+  </script>
+</body>
+</html>
